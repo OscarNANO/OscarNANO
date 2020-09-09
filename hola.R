1e6ac6321b25419e7851d312c73ffabf11f020a0
OscarNANO <70617234+OscarNANO@users.noreply.github.com>
2020-09-09 12:20:15 -0500

    Add files via upload

---
+++ hola.R
@@ -0,0 +1,31 @@
+print ("hola")
+print ("estoy aprendiendo R")
+nt ("hola")
+[1] "hola"
+> print ("estoy aprendiendo R")
+[1] "estoy aprendiendo R"
+> print ("numeros")
+[1] "numeros"
+> print ("lista")
+[1] "lista"
+> print ("1, 2, 3, 4, 5, 6, 7, 8, 9")
+[1] "1, 2, 3, 4, 5, 6, 7, 8, 9"
+> print (lista "1, 2, 3, 4, 5, 6, 7, 8, 9")
+Error: unexpected string constant in "print (lista "1, 2, 3, 4, 5, 6, 7, 8, 9""
+> lista ("1, 2, 3, 4, 5, 6, 7, 8, 9")
+Error in lista("1, 2, 3, 4, 5, 6, 7, 8, 9") : 
+  no se pudo encontrar la función "lista"
+> lista = ("1, 2, 3, 4, 5, 6, 7, 8, 9") 
+> print lista
+Error: unexpected symbol in "print lista"
+> print("lista")
+[1] "lista"
+> print ("texto")
+[1] "texto"
+> sum ("lista")
+Error in sum("lista") : 'type' (character) de argumento no válido
+> lista
+[1] "1, 2, 3, 4, 5, 6, 7, 8, 9"
+> sum ("lista")
+>  print ("intentar guardar")
+[1] "intentar guardar"
---
+++ hola.py
@@ -0,0 +1,46 @@
+
+# este codigo es mi titulo
+print ("hola amigos")
+
+# cadena
+print ("hola amigos")
+print ("buenos dias")
+print ("bienvendos")
+print ("primer" + "intento")
+
+# numeros
+# enteros
+print (123)
+print (888)
+print (1842398481291)
+
+# flotantes
+print (1.5)
+print (38.4)
+print (8723.495)
+
+# boolean
+verdadero
+falso
+
+# lista
+[2, 4, 6, 8, 10, 12]
+[rojo, blanco, azul, morado, verde]
+[12.3, 45.1, 8.33, 96.7]
+[rojo, 6, verde, 8.33]
+
+# tuplas
+[12, 55, 97, 113]
+
+# condicionales
+x = 18
+if x < 18: 
+    print ("niño")
+else x > 18: 
+    print ("adulto")
+
+color = "azul"
+if color == "azul":
+    print ("el color es azul")
+else:
+    print: ("es otro color")    
